d7eab1a1fc24a1973d6cb1b3b2df4e3719994851
ngophuongha <ngophuongha3105@gmail.com>
2022-04-07 15:24:28 +0700

    install plugin

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -1,13 +1,13 @@
-export * from "./common";
-export * from "./light";
-export * from "./dark";
-
 import { FC } from "react";
 import { ThemeProvider } from "styled-components";
 import { useAppSelector } from "../store/hooks";
 import { dark } from "./dark";
 import { light } from "./light";
 import { ThemeType } from "../store/theme/theme";
+
+export * from "./common";
+export * from "./light";
+export * from "./dark";
 
 export const StyledThemeProvider: FC = ({ children }) => {
   const currentTheme = useAppSelector((state) => state.theme.theme);
