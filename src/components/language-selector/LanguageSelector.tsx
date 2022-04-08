2d95f3cf7d23425d2bf457285d076d3697dfa034
ngophuongha <ngophuongha3105@gmail.com>
2022-04-08 12:54:10 +0700

    remove node module and npm i

--- src/components/language-selector/LanguageSelector.tsx
+++ src/components/language-selector/LanguageSelector.tsx
@@ -21,7 +21,6 @@ export const LanguageSelector = (): JSX.Element => {
       dispatch(languageChange(lang));
       i18n.changeLanguage(lang);
     },
-    //eslint-disable-next-line react-hooks/exhaustive-deps
     [currentLanguage]
   );
 
